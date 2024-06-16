6c85505f80b082d48e4b63c9d003d0f138ad3a5a
Hugo Arcanjo <hfarcanjo@gmail.com>
2024-06-16 09:32:36 -0300

    feat: added exchange rate api connection

--- src/ExchangeRate.java
+++ src/ExchangeRate.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class ExchangeRate {
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ExchangeRate {
+
+    public Currency getCurrentExchangeRate(String currencyFrom, String currencyTo) {
+
+        URI serviceURL = URI.create("https://v6.exchangerate-api.com/v6/a7d9d0e70db88132d53e2114/pair/" + currencyFrom + "/" + currencyTo);
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(serviceURL)
+                .build();
+
+        HttpResponse<String> response = null;
+        try {
+            response = HttpClient
+                    .newHttpClient()
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        return new Gson().fromJson(response.body(), Currency.class);
+    }
 }
