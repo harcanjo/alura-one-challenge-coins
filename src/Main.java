59c78ed47730ea218eec9528071a17e9940a8487
Hugo Arcanjo <hfarcanjo@gmail.com>
2024-06-16 06:34:42 -0300

    feat: added menu and user input

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,61 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int exit = 1;
+
+        while(exit != 0) {
+
+            System.out.println("*******************************************************");
+            System.out.println("Seja bem-vindo/a ao Conversor de Moeda =]\n");
+            System.out.println("1) Dólar ==> Peso argentino");
+            System.out.println("2) Peso argentino ==> Dólar");
+            System.out.println("3) Dólar ==> Real brasileiro");
+            System.out.println("4) Real brasileiro ==> Dólar");
+            System.out.println("5) Dólar ==> Peso colombiano");
+            System.out.println("6) Peso colombiano ==> Dólar");
+            System.out.println("7) Sair");
+            System.out.println("Escolha uma opção válida: ");
+            System.out.println("*******************************************************");
+
+            int option = scanner.nextInt();
+
+            switch (option) {
+                case 1:
+                    System.out.println("1");
+                    break;
+                case 2:
+                    System.out.println("2");
+                    break;
+                case 3:
+                    System.out.println("3");
+                    break;
+                case 4:
+                    System.out.println("4");
+                    break;
+                case 5:
+                    System.out.println("5");
+                    break;
+                case 6:
+                    System.out.println("6");
+                    break;
+                case 7:
+                    System.out.println("7");
+                    exit = 0;
+                    break;
+                default:
+                    System.out.println("Opção inválida");
+                    break;
+            }
+
+            System.out.println("Digite o valor que deseja converter: ");
+            double valueToConvert = scanner.nextDouble();
+
+            System.out.println("Valor " + valueToConvert +
+                    " [" + option + "] corresponde ao valor final de ==> " +
+                    valueToConvert * option + " [" + option+ "] ");
         }
     }
 }
